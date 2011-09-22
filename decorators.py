c8b740a4e491cf1f13846abf22d9f4dc41d67082
d1ffuz0r <gladk0w@mail.ru>
2011-09-22 21:46:18 +0400

    fix comment

--- decorators.py
+++ decorators.py
@@ -3,9 +3,9 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def render_to(template):
-    '''
+    """
     decorator for rendering views to template
-    '''
+    """
     def renderer(func):
         @wraps(func)
         def wrapper(request, *args, **kw):
